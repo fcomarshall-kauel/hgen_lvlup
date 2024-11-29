6bdc76ccf9b0df682fefec56b63fc45f5b354cfe
fcomarshall-kauel <136859735+fcomarshall-kauel@users.noreply.github.com>
2024-11-29 01:05:56 -0300

    additional information , fixed

--- app/api/ai-chat/route.ts
+++ app/api/ai-chat/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   try {
     const { message } = await request.json();
+    const additional_information = "Hoy interactuarás con el Dr. Sergio Cedano Rivera, un médico internista de Integramédica en Providencia, Santiago, quien tiene 64 años. Al dirigirte a él, utiliza un tono formal casual y aprovecha sus intereses personales para generar confianza. El Dr. Cedano disfruta del ajedrez, ama tanto la montaña como el mar y colecciona motocicletas. Puedes preguntarle sobre su última partida de ajedrez o si ha añadido alguna motocicleta especial a su colección. Asegúrate de felicitarlo por su cumpleaños el 31 de enero y menciona su gusto por la comida italiana y el jazz. Además, ofrece servicios de telemedicina y trabaja con la Isapre Banmédica, lo cual podría ser relevante para la conversación.";
 
     const response = await fetch(
-      `https://server.ai-labhelper.com/api/chat?session_id=666288c3bbdeec47501c57e2&message=${encodeURIComponent(message)}&thread_id=conversacion1`,
+      `https://server.ai-labhelper.com/api/chat?session_id=666288c3bbdeec47501c57e2&message=${encodeURIComponent(message)}&thread_id=conversacion1&additional_information=${encodeURIComponent(additional_information)}`,
       {
         method: 'POST',
         headers: {
